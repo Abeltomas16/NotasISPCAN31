42956269bb0f34b7d73d28df1dd38b5653ed3693
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:57:02 +0100

    FEAT: add service notas

---
+++ NotasISPCAN/NotasISPCAN/Features/Service/LoadingPageService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace NotasISPCAN.Features.Service
+{
+    public class LoadingPageService : IAuthenticationService
+    {
+        public async Task<bool> IsLogged()
+        {
+            string categoria = Preferences.Get("Categoria", "doesnot");
+            string usuario = Preferences.Get("MyFirebaseToken", "doesnot");
+            string email = Preferences.Get("MyEmail", "doesnot");
+            string senha = Preferences.Get("MySenha", "doesnot");
+            await Task.Delay(500);
+            return usuario != "doesnot" || email != "doesnot" || senha != "doesnot" || categoria != "doesnot";
+        }
+    }
+}
