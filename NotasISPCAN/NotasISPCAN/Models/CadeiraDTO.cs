b8c6da85ea4c2dcc9ed62d7b764d59f8f5f3554e
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:11:42 +0100

    FEAT: add model cadeira e nota

---
+++ NotasISPCAN/NotasISPCAN/Models/CadeiraDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotasISPCAN.Models
+{
+    public class CadeiraDTO
+    {
+        public string IDCadeira { get; set; }
+        public string Name { get; set; }
+        public string Docente { get; set; }
+    }
+}
