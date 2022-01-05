79068f56df5f6ed88d5b82cb5b67f9579755ec2d
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:59:03 +0100

    FEAT: add service estudante

---
+++ NotasISPCAN/NotasISPCAN/Features/Service/EstudanteService.cs
@@ -0,0 +1,21 @@
+﻿using NotasISPCAN.Models;
+using Splat;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NotasISPCAN.Features.Service
+{
+    public class EstudanteService : IEstudante
+    {
+        INotas notasService;
+        public EstudanteService(INotas notas = null)
+        {
+            notasService = notas ?? Locator.Current.GetService<INotas>();
+        }
+        public async Task<List<NotasCadeirasDocente>> ListarNotas(string Keyaluno)
+        {
+            var notas = await notasService.listarPorAluno(Keyaluno);
+            return notas;
+        }
+    }
+}
