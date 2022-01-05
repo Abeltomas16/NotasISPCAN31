f5066e78769fb69bdbc9a3b83cef1891b8917c76
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:28:13 +0100

    FEAt: add validação do login

---
+++ NotasISPCAN/NotasISPCAN/Commom/Validation/LoginValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Models;
+
+namespace NotasISPCAN.Commom.Validation
+{
+    public class LoginValidator : AbstractValidator<Login>
+    {
+        public LoginValidator()
+        {
+            RuleFor(e => e.email)
+                .EmailAddress().WithErrorCode(statusCode.EmailInvalid);
+
+            RuleFor(s => s.Senha)
+                .NotNull()
+                .NotEmpty().WithErrorCode(statusCode.SenhaIsNullOrEmpty);
+        }
+    }
+}
