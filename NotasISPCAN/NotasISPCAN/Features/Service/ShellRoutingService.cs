7fff38688619bcca59ce3cd9bcc67c074ea78dcb
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:56:11 +0100

    FEAT: add service rota

---
+++ NotasISPCAN/NotasISPCAN/Features/Service/ShellRoutingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NotasISPCAN.Features.Service
+{
+    public class IRoutingService : IRouting
+    {
+        public IRoutingService()
+        {
+        }
+        public Task NavigateTo(string route)
+        {
+            return Shell.Current.GoToAsync(route);
+        }
+        public Task GoBack()
+        {
+            return Shell.Current.Navigation.PopAsync();
+        }
+        public Task GoBackModal()
+        {
+            return Shell.Current.Navigation.PopModalAsync();
+        }
+        Task IRouting.GoBack()
+        {
+            throw new NotImplementedException();
+        }
+        Task IRouting.GoBackModal()
+        {
+            throw new NotImplementedException();
+        }
+        Task IRouting.NavigateTo(string route)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
