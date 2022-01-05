e82ced509ae1d618f3c75a900334af2af9f83de9
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2022-01-05 21:13:03 +0100

    FEAT: add view model notas e usuario

---
+++ NotasISPCAN/NotasISPCAN/ViewModel/NotasViewModel.cs
@@ -0,0 +1,152 @@
+﻿using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Commom.Validation;
+using NotasISPCAN.Features.Service;
+using NotasISPCAN.Models;
+using Splat;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XF.Material.Forms.UI.Dialogs;
+
+namespace NotasISPCAN.ViewModel
+{
+    public class NotasViewModel : BaseViewModel
+    {
+        INotas notasService;
+        NotasValidar Validations;
+        public NotasViewModel(string estudantekey, INotas notas = null)
+        {
+            notasService = notas ?? Locator.Current.GetService<INotas>();
+            Cadastrar = new Command(() => Salvar());
+            cadeiraKey = Application.Current.Properties["IDCadeira"].ToString();
+            Validations = Locator.Current.GetService<NotasValidar>();
+            EstudanteKey = estudantekey;
+            Task.Run(async () => await CarregaNotas(estudantekey));
+        }
+        private async Task CarregaNotas(string estudantekey)
+        {
+            NotasDTO nota = await notasService.listarPorCadeira(cadeiraKey, estudantekey);
+            IDNota = nota.Key ?? null;
+            _Nota1 = nota.Nota1 ?? null;
+            _Nota2 = nota.Nota2 ?? null;
+            OnPropertyChanged("_Nota1");
+            OnPropertyChanged("_Nota2");
+        }
+        private string EstudanteKey { get; set; }
+        private string cadeiraKey { get; set; }
+        public string _Nota1 { get; set; }
+        public string _Nota2 { get; set; }
+        private string IDNota { get; set; }
+        public ICommand Cadastrar { get; set; }
+        private async void Salvar()
+        {
+            string retorno = string.Empty;
+            NotasDTO notas = new NotasDTO
+            {
+                Nota1 = _Nota1 ?? null,
+                Nota2 = _Nota2 ?? null,
+                KeyCadeira = cadeiraKey,
+                KeyAluno = EstudanteKey
+            };
+
+            if (string.IsNullOrEmpty(IDNota))
+            {
+                string verifica = ValidateInsert(notas);
+                if (verifica != null)
+                {
+                    await ExibeMensagem(verifica);
+                    return;
+                };
+                retorno = await notasService.Cadastrar(notas);
+                await ExibeMensagem(retorno);
+            }
+            else
+            {
+                notas.Key = IDNota;
+                string verifica = ValidateUpdate(notas);
+                if (verifica != null)
+                {
+                    await ExibeMensagem(verifica);
+                    return;
+                };
+                retorno = await notasService.Alterar(notas);
+                await ExibeMensagem(retorno);
+            }
+
+        }
+        public async Task ExibeMensagem(string retono)
+        {
+            await MaterialDialog.Instance.SnackbarAsync(message: retono, actionButtonText: "Ok", msDuration: MaterialSnackbar.DurationLong,
+                  new XF.Material.Forms.UI.Dialogs.Configurations.MaterialSnackbarConfiguration
+                  {
+                      BackgroundColor = System.Drawing.Color.Orange,
+                      MessageTextColor = System.Drawing.Color.Black
+                  });
+        }
+        public string ValidateInsert(NotasDTO notas)
+        {
+            string retorno = null;
+            var results = Validations.Validate(notas);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorMessage == statusCode.IdAlunoNotNull)
+                        retorno = "Informe o aluno";
+                    if (item.ErrorCode == statusCode.IdCadeiraNotNull)
+                        retorno = "Informe a cadeira";
+                    if (item.ErrorCode == statusCode.Nota1NotNull)
+                        retorno = "Nota 1 não pode ser nula";
+                    if (item.ErrorCode == statusCode.NotaInvalid)
+                        retorno = "Nota 1 inválida";
+                    if (item.ErrorCode == statusCode.Nota1MaiorQue20)
+                        retorno = "Nota 1 não pode ser superior a 20";
+                    if (item.ErrorCode == statusCode.NotaMenorZero)
+                        retorno = "Nota não pode ser menor que 0";
+
+                    if (item.ErrorCode == statusCode.Nota2NotNull)
+                        retorno = "Nota 2 não pode ser nula";
+                    if (item.ErrorCode == statusCode.Nota2MaiorQue20)
+                        retorno = "Nota 2 não pode ser superior a 20";
+                    if (item.ErrorCode == statusCode.NotaMenorZero)
+                        retorno = "Nota não pode ser menor que 0";
+                }
+            }
+            return retorno;
+        }
+        public string ValidateUpdate(NotasDTO notas)
+        {
+            string retorno = null;
+            var results = Validations.Validate(notas);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorMessage == statusCode.IdDeveSerInformado)
+                        retorno = "Informe o id";
+
+                    if (item.ErrorMessage == statusCode.IdAlunoNotNull)
+                        retorno = "Informe o aluno";
+                    if (item.ErrorCode == statusCode.IdCadeiraNotNull)
+                        retorno = "Informe a cadeira";
+                    if (item.ErrorCode == statusCode.Nota1NotNull)
+                        retorno = "Nota 1 não pode ser nula";
+                    if (item.ErrorCode == statusCode.NotaInvalid)
+                        retorno = "Nota 1 inválida";
+                    if (item.ErrorCode == statusCode.Nota1MaiorQue20)
+                        retorno = "Nota 1 não pode ser superior a 20";
+                    if (item.ErrorCode == statusCode.NotaMenorZero)
+                        retorno = "Nota não pode ser menor que 0";
+
+                    if (item.ErrorCode == statusCode.Nota2NotNull)
+                        retorno = "Nota 2 não pode ser nula";
+                    if (item.ErrorCode == statusCode.Nota2MaiorQue20)
+                        retorno = "Nota 2 não pode ser superior a 20";
+                    if (item.ErrorCode == statusCode.NotaMenorZero)
+                        retorno = "Nota não pode ser menor que 0";
+                }
+            }
+            return retorno;
+        }
+    }
+}
---
+++ NotasISPCAN/NotasISPCAN/ViewModel/UsuarioViewModel.cs
@@ -0,0 +1,154 @@
+﻿using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Commom.Validation;
+using NotasISPCAN.Features.Service;
+using NotasISPCAN.Models;
+using Splat;
+using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using XF.Material.Forms.UI.Dialogs;
+
+namespace NotasISPCAN.ViewModel
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        IUsuario Idata;
+        Usuariovalidator Validations;
+        public UsuarioViewModel(IUsuario usuario = null)
+        {
+            Idata = usuario ?? Locator.Current.GetService<IUsuario>();
+            Validations = Locator.Current.GetService<Usuariovalidator>();
+            Carregar();
+        }
+        public int Total { get; private set; }
+        public ObservableCollection<UsuarioDTO> Estudantes { get; set; }
+        public async void Carregar()
+        {
+            IMaterialModalPage load = null;
+            try
+            {
+                load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Carregando");
+                var estudantes = await Idata.ListarTodos();
+                Estudantes = new ObservableCollection<UsuarioDTO>(estudantes.Where(y => y.Categoria == "Estudante"));
+                OnPropertyChanged("Estudantes");
+                Total = Estudantes.Count;
+                OnPropertyChanged("Total");
+                await load.DismissAsync();
+            }
+            catch (Exception)
+            {
+                await MaterialDialog.Instance.SnackbarAsync(message: "Erro, contacte o administrador", actionButtonText: "Ok", msDuration: MaterialSnackbar.DurationLong,
+                 new XF.Material.Forms.UI.Dialogs.Configurations.MaterialSnackbarConfiguration
+                 {
+                     BackgroundColor = Color.Orange,
+                     MessageTextColor = Color.Black
+                 });
+            }
+            finally
+            {
+                if (!(load is null))
+                    await load.DismissAsync();
+            }
+        }
+        public async Task<string> Cadastrar(UsuarioDTO usuario)
+        {
+            var results = Validations.Validate(usuario);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.NomeIsNullOrEmpty ||
+                        item.ErrorCode == statusCode.NomeNotCaractereEspecial)
+                        return "Nome inválido";
+                    if (item.ErrorCode == statusCode.TelefoneIsNotNullOrEmpty)
+                        return "Telefone inválido";
+                    if (item.ErrorCode == statusCode.TelefoneCaractereMinimo)
+                        return "Telefone requer 9 digítos";
+                    if (item.ErrorCode == statusCode.EmailInvalid)
+                        return "Email inválido";
+                    if (item.ErrorCode == statusCode.CategoriaInvalido)
+                        return "categoria inválida";
+                    if (item.ErrorCode == statusCode.SenhaIsNullOrEmpty)
+                        return "Informe a senha";
+                    if (item.ErrorCode == statusCode.SenhaCaractereMinimo)
+                        return "A senha tem que 4 caracteres no minímo";
+                }
+            }
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Salvando");
+            var retorno = await Idata.Cadastrar(usuario);
+            await load.DismissAsync();
+            return retorno;
+        }
+        public async Task<string> Alterar(UsuarioDTO usuario)
+        {
+            var results = Validations.Validate(usuario);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.NomeIsNullOrEmpty ||
+                        item.ErrorCode == statusCode.NomeNotCaractereEspecial)
+                        return "Nome inválido";
+                    if (item.ErrorCode == statusCode.TelefoneIsNotNullOrEmpty)
+                        return "Telefone inválido";
+                    if (item.ErrorCode == statusCode.TelefoneCaractereMinimo)
+                        return "Telefone requer 9 digítos";
+                    if (item.ErrorCode == statusCode.EmailInvalid)
+                        return "Email inválido";
+                    if (item.ErrorCode == statusCode.CategoriaInvalido)
+                        return "categoria inválida";
+                    if (item.ErrorCode == statusCode.SenhaIsNullOrEmpty)
+                        return "Informe a senha";
+                    if (item.ErrorCode == statusCode.SenhaCaractereMinimo)
+                        return "A senha tem que 4 caracteres no minímo";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        return "Informe o id";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        return "Informe o token";
+                }
+            }
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Actualizando");
+            var retorno = await Idata.Alterar(usuario, usuario.Key);
+            await load.DismissAsync();
+            return retorno;
+        }
+        public async Task<string> Apagar(UsuarioDTO usuarioDTO)
+        {
+            var results = Validations.Validate(usuarioDTO);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.EmailInvalid)
+                        return "Email inválido";
+                    if (item.ErrorCode == statusCode.CategoriaInvalido)
+                        return "categoria inválida";
+                    if (item.ErrorCode == statusCode.SenhaIsNullOrEmpty)
+                        return "Informe a senha";
+                    if (item.ErrorCode == statusCode.SenhaCaractereMinimo)
+                        return "A senha tem que 4 caracteres no minímo";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        return "Informe o id";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        return "Informe o token";
+                }
+            }
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Apagando");
+            var resultado = await Idata.Apagar(usuarioDTO);
+            await load.DismissAsync();
+            return resultado;
+        }
+        public async Task<string> UpdateEmail(string newEmail)
+        {
+            string retorno = await Idata.AlterarEmail(newEmail);
+            return retorno;
+        }
+        public async Task<string> UpdateSenha(string newSenha)
+        {
+            string retorno = await Idata.AlterarSenha(newSenha);
+            return retorno;
+        }
+    }
+}
