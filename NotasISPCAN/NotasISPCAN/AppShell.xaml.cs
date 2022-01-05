46a5543294676727f44ecb80e4b3f4481354de57
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2022-01-05 22:46:24 +0100

    FEAT: add shell page

---
+++ NotasISPCAN/NotasISPCAN/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotasISPCAN
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
