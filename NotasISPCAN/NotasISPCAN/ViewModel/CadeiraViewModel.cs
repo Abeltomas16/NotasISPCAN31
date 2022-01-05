9f0c7f0fe2d247bd9d594ac5c7d81ec8c85fd1af
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2022-01-05 21:03:17 +0100

    FEAT: add view model docente e cadeira

---
+++ NotasISPCAN/NotasISPCAN/ViewModel/CadeiraViewModel.cs
@@ -0,0 +1,117 @@
+﻿using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Commom.Validation;
+using NotasISPCAN.Features.Enums;
+using NotasISPCAN.Features.Service;
+using NotasISPCAN.Models;
+using Splat;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using XF.Material.Forms.UI.Dialogs;
+
+namespace NotasISPCAN.ViewModel
+{
+    public class CadeiraViewModel : BaseViewModel
+    {
+        //Total de cadeiras existentes
+        public int Total { get; private set; }
+        ICadeira cadeiraService = Locator.Current.GetService<ICadeira>();
+        CadeiraValidator Validations;
+        public CadeiraViewModel(string Tela)
+        {
+            Validations = Locator.Current.GetService<CadeiraValidator>();
+            if (Tela == "CadeirasMostrar")
+                Task.Run(async () => await CarregarCadeiraLivres());
+            else  //QUANDO A TELA NÃO FOR CADEIRA MOSTRAR, LOGO É A TELA DE INSERÇÃO DAS CADEIRAS
+                Task.Run(async () => await Carregar());
+        }
+        public CadeiraViewModel(string Tela, List<CadeiraDTO> cadeiras)
+        {
+            Validations = Locator.Current.GetService<CadeiraValidator>();
+            //Depois de pegar todas cadeiras, seleciono todas em que um pof leciona
+            selectedCadeira = new ObservableCollection<object>();
+            Task.Run(async () => await Carregar())
+                .ContinueWith(
+                tarefaAnterior =>
+                {
+                    for (int i = 0; i < Cadeiras.Count; i++)
+                    {
+                        for (int j = 0; j < cadeiras.Count; j++)
+                        {
+                            if (Cadeiras[i].IDCadeira == cadeiras[j].IDCadeira)
+                                SelectedCadeira.Add(Cadeiras[i]);
+                        }
+                    }
+                },
+                TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+        ObservableCollection<object> selectedCadeira;
+        public ObservableCollection<object> SelectedCadeira
+        {
+            get
+            {
+                return selectedCadeira;
+            }
+            set
+            {
+                if (selectedCadeira != value)
+                {
+                    selectedCadeira = value;
+                    OnPropertyChanged("selectedCadeira");
+                }
+            }
+        }
+        public ObservableCollection<CadeiraDTO> Cadeiras { get; set; }
+        public async Task<string> CadastrarEditar(CadeiraDTO cadeira, EnumOperacoes operacao)
+        {
+            var results = Validations.Validate(cadeira);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.DescricaoCaractereMinimo11 ||
+                        item.ErrorCode == statusCode.DescricaoIsNotNullOrEmpty ||
+                        item.ErrorCode == statusCode.DescricaoNotCaracteresEspecial)
+                        return "Descrição inválida";
+                    if (operacao == EnumOperacoes.Editar)
+                        if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                            return "Id da cadeira deve ser informado";
+                }
+            }
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Salvando");
+            var retorno = await cadeiraService.inserirOuAlterar(cadeira, operacao);
+            await load.DismissAsync();
+            return retorno;
+        }
+        public async Task Carregar()
+        {
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Carregando");
+            var cadeiras = await cadeiraService.listarTodos();
+            this.Cadeiras = new ObservableCollection<CadeiraDTO>(cadeiras);
+            OnPropertyChanged("Cadeiras");
+            Total = this.Cadeiras.Count;
+            OnPropertyChanged("Total");
+            await load.DismissAsync();
+        }
+        public async Task<string> Apagar(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return "Id invalido";
+
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Apagando");
+            var resultado = await cadeiraService.apagar(id);
+            await load.DismissAsync();
+            return resultado;
+        }
+        public async Task CarregarCadeiraLivres()
+        {
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Carregando");
+            var cadeiras = await cadeiraService.CadeiraLivre();
+            this.Cadeiras = new ObservableCollection<CadeiraDTO>(cadeiras);
+            OnPropertyChanged("Cadeiras");
+            Total = this.Cadeiras.Count;
+            OnPropertyChanged("Total");
+            await load.DismissAsync();
+        }
+    }
+}
---
+++ NotasISPCAN/NotasISPCAN/ViewModel/DocenteViewModel.cs
@@ -0,0 +1,232 @@
+﻿using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Commom.Validation;
+using NotasISPCAN.Features.Service;
+using NotasISPCAN.Models;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using XF.Material.Forms.UI.Dialogs;
+
+namespace NotasISPCAN.ViewModel
+{
+    public class DocenteViewModel : BaseViewModel
+    {
+        private CadeiraDTO CadeiraSelecionada = null;
+        public int Total { get; private set; }
+        public CadeiraDTO RetornaCadeiraSelecionada
+        {
+            get { return CadeiraSelecionada; }
+            set
+            {
+                if (CadeiraSelecionada != value)
+                {
+                    CadeiraSelecionada = value;
+                }
+            }
+        }
+        ICadeira clienteCadeira;
+        IDocente clienteDocente;
+        IUsuario clienteUsuario;
+        Usuariovalidator Validations;
+        CadeiraValidator ValidationsCadeira;
+        public DocenteViewModel(IDocente docente = null, IUsuario usuario = null, ICadeira cadeira = null)
+        {
+            clienteDocente = docente ?? Locator.Current.GetService<IDocente>();
+            clienteCadeira = cadeira ?? Locator.Current.GetService<ICadeira>();
+            clienteUsuario = usuario ?? Locator.Current.GetService<IUsuario>();
+            ValidationsCadeira = Locator.Current.GetService<CadeiraValidator>();
+            Validations = Locator.Current.GetService<Usuariovalidator>();
+            Task.Run(async () => await Carregar());
+        }
+        public ObservableCollection<UsuarioDTO> Docentes { get; set; }
+        public ObservableCollection<CadeiraDTO> Cadeiras { get; set; }
+        public async Task<string> Cadastrar(UsuarioDTO docente, List<CadeiraDTO> cadeiras)
+        {
+            string retornos = ValidarInsert(docente, cadeiras);
+            if (retornos != null) return retornos;
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Salvando");
+            var retorno = await clienteDocente.Cadastrar(docente, cadeiras);
+            await load.DismissAsync();
+            return retorno;
+        }
+        public async Task<string> Alterar(UsuarioDTO docente, List<CadeiraDTO> cadeiras)
+        {
+            string retornos = ValidarUpdate(docente, cadeiras);
+            if (retornos != null) return retornos;
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Actualizando");
+            var retorno = await clienteDocente.Alterar(docente, cadeiras);
+            await load.DismissAsync();
+            return retorno;
+        }
+        public async Task Carregar()
+        {
+            IMaterialModalPage load = null;
+            try
+            {
+                load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Carregando");
+                var docentes = await clienteDocente.ListarTodos();
+                Docentes = new ObservableCollection<UsuarioDTO>(docentes);
+                OnPropertyChanged("Docentes");
+                Total = Docentes.Count;
+                OnPropertyChanged("Total");
+
+                var cadeira = await clienteCadeira.listarTodos();
+                Cadeiras = new ObservableCollection<CadeiraDTO>(cadeira);
+                OnPropertyChanged("Cadeiras");
+                await load.DismissAsync();
+            }
+            catch (Exception)
+            {
+                if (!(load is null))
+                    await load.DismissAsync();
+            }
+        }
+
+        public async Task<UsuarioDTO> MelhorAluno(string keyMelhorAluno)
+        {
+            var usuario = await clienteUsuario.PesquisarPorKey(keyMelhorAluno);
+            return usuario;
+        }
+
+        public async Task<List<CadeiraDTO>> MostrarCadeira(string key)
+        {
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Carregando");
+            var cadeiras = await clienteCadeira.MostrarPorID(key);
+            await load.DismissAsync();
+            return cadeiras;
+        }
+        public async Task<string> Apagar(UsuarioDTO key)
+        {
+            string retorno = ValidarDelete(key);
+            if (retorno != null) return retorno;
+            var load = await MaterialDialog.Instance.LoadingDialogAsync(message: "Apagando");
+            var resultado = await clienteDocente.Apagar(key);
+            await load.DismissAsync();
+            return resultado;
+        }
+        public async Task<List<NotasDTO>> mostrarNotas(string keycadeira)
+        {
+            var resultado = await clienteDocente.MostrarNotas(keycadeira);
+            return resultado;
+        }
+        public async Task<List<NotasDTO>> mostrarNotas()
+        {
+            var resultado = await clienteDocente.MostrarNotas();
+            return resultado;
+        }
+        public string ValidarInsert(UsuarioDTO docente, List<CadeiraDTO> cadeiras)
+        {
+            string retorno = null;
+            var results = Validations.Validate(docente);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.NomeIsNullOrEmpty ||
+                        item.ErrorCode == statusCode.NomeNotCaractereEspecial)
+                        retorno = "Nome inválido";
+                    if (item.ErrorCode == statusCode.TelefoneIsNotNullOrEmpty)
+                        retorno = "Telefone inválido";
+                    if (item.ErrorCode == statusCode.TelefoneCaractereMinimo)
+                        retorno = "Telefone requer 9 digítos";
+                    if (item.ErrorCode == statusCode.EmailInvalid)
+                        retorno = "Email inválido";
+                    if (item.ErrorCode == statusCode.CategoriaInvalido)
+                        retorno = "categoria inválida";
+                    if (item.ErrorCode == statusCode.SenhaIsNullOrEmpty)
+                        retorno = "Informe a senha";
+                    if (item.ErrorCode == statusCode.SenhaCaractereMinimo)
+                        retorno = "A senha tem que 4 caracteres no minímo";
+                }
+            }
+            if (cadeiras.Count > 0)
+            {
+                foreach (var item in cadeiras)
+                {
+                    var results2 = ValidationsCadeira.Validate(item);
+                    if (!results2.IsValid)
+                    {
+                        foreach (var item2 in results2.Errors)
+                        {
+                            if (item2.ErrorCode == statusCode.IdDeveSerInformado)
+                                retorno = "Informe o id";
+                        }
+                    }
+                }
+            }
+            return retorno;
+        }
+        public string ValidarUpdate(UsuarioDTO docente, List<CadeiraDTO> cadeiras)
+        {
+            string retorno = null;
+            var results = Validations.Validate(docente);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.NomeIsNullOrEmpty ||
+                        item.ErrorCode == statusCode.NomeNotCaractereEspecial)
+                        retorno = "Nome inválido";
+                    if (item.ErrorCode == statusCode.TelefoneIsNotNullOrEmpty)
+                        retorno = "Telefone inválido";
+                    if (item.ErrorCode == statusCode.TelefoneCaractereMinimo)
+                        retorno = "Telefone requer 9 digítos";
+                    if (item.ErrorCode == statusCode.EmailInvalid)
+                        retorno = "Email inválido";
+                    if (item.ErrorCode == statusCode.CategoriaInvalido)
+                        retorno = "categoria inválida";
+                    if (item.ErrorCode == statusCode.SenhaIsNullOrEmpty)
+                        retorno = "Informe a senha";
+                    if (item.ErrorCode == statusCode.SenhaCaractereMinimo)
+                        retorno = "A senha tem que 4 caracteres no minímo";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        retorno = "Informe o id";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        retorno = "Informe o token";
+                }
+            }
+            if (cadeiras.Count > 0)
+            {
+                foreach (var item in cadeiras)
+                {
+                    var results2 = ValidationsCadeira.Validate(item);
+                    if (!results2.IsValid)
+                    {
+                        foreach (var item2 in results2.Errors)
+                        {
+                            if (item2.ErrorCode == statusCode.IdDeveSerInformado)
+                                retorno = "Informe o id";
+                        }
+                    }
+                }
+            }
+            return retorno;
+        }
+        public string ValidarDelete(UsuarioDTO key)
+        {
+            string retorno = null;
+            var results = Validations.Validate(key);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    if (item.ErrorCode == statusCode.EmailInvalid)
+                        retorno = "Email inválido";
+                    if (item.ErrorCode == statusCode.CategoriaInvalido)
+                        retorno = "categoria inválida";
+                    if (item.ErrorCode == statusCode.SenhaIsNullOrEmpty)
+                        retorno = "Informe a senha";
+                    if (item.ErrorCode == statusCode.SenhaCaractereMinimo)
+                        retorno = "A senha tem que 4 caracteres no minímo";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        retorno = "Informe o id";
+                    if (item.ErrorCode == statusCode.IdDeveSerInformado)
+                        retorno = "Informe o token";
+                }
+            }
+            return retorno;
+        }
+    }
+}
