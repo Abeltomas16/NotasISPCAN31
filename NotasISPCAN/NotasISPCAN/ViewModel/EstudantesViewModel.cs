a3cfef9f4675e30a927beac65763985dff8ec16b
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2022-01-05 21:05:36 +0100

    FEAT: add view model estudante e home

---
+++ NotasISPCAN/NotasISPCAN/ViewModel/EstudantesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using NotasISPCAN.Features.Service;
+using NotasISPCAN.Models;
+using Splat;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NotasISPCAN.ViewModel
+{
+    public class EstudantesViewModel
+    {
+        IEstudante estudanteService;
+        public EstudantesViewModel(IEstudante estudantes = null)
+        {
+            estudanteService = estudantes ?? Locator.Current.GetService<IEstudante>();
+        }
+        public async Task<List<NotasCadeirasDocente>> listar(string keyEstudante)
+        {
+            var notas = await estudanteService.ListarNotas(keyEstudante);
+            return notas;
+        }
+    }
+}
---
+++ NotasISPCAN/NotasISPCAN/ViewModel/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NotasISPCAN.Features.Service;
+using Xamarin.Forms;
+
+namespace NotasISPCAN.ViewModel
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+            VerificaaLogin();
+        }
+        private async void VerificaaLogin()
+        {
+            await Shell.Current.GoToAsync("//LoginView");
+        }
+    }
+}
