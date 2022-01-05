50ce54a5c6f401389f1bed44d546735ae3486b80
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:25:43 +0100

    FEAT: add recursos e validação da actividade

---
+++ NotasISPCAN/NotasISPCAN/Commom/Validation/ActividadesValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Models;
+
+namespace NotasISPCAN.Commom.Validation
+{
+    public class ActividadesValidator : AbstractValidator<ActividadeDTO>
+    {
+        public ActividadesValidator()
+        {
+            RuleFor(e => e.Descricao)
+            .NotNull()
+            .NotEmpty().WithErrorCode(statusCode.DescricaoIsNotNullOrEmpty)
+            .Must((desc) =>
+            {
+                bool convertido = int.TryParse(desc, out int result) ? false : true;
+                return convertido;
+            }).WithErrorCode(statusCode.NaoPodeSerApenasNumero);
+
+            RuleFor(z => z.IDActividade)
+                 .NotNull()
+                .NotEmpty().WithErrorCode(statusCode.IdDeveSerInformado);
+        }
+    }
+}
