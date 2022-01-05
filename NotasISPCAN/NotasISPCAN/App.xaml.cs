b5bfbd8e524ceaa4a488c1e10863aaa9116100c2
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:05:41 +0100

    Adicionar arquivos de projeto.

---
+++ NotasISPCAN/NotasISPCAN/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using NotasISPCAN.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotasISPCAN
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Page1());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
