d371cf5e0056313784c12dc9cc97aa36732eb0e0
Abel Tomás <abeljosetoms115@gmail.com>
2022-01-05 16:27:07 +0100

    FEAt: add validação da cadeira

---
+++ NotasISPCAN/NotasISPCAN/Commom/Validation/CadeiraValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using NotasISPCAN.Commom.Resources;
+using NotasISPCAN.Models;
+
+namespace NotasISPCAN.Commom.Validation
+{
+    public class CadeiraValidator : AbstractValidator<CadeiraDTO>
+    {
+        public CadeiraValidator()
+        {
+            RuleFor(e => e.Name)
+                .NotNull()
+                .NotEmpty().WithErrorCode(statusCode.DescricaoIsNotNullOrEmpty)
+                .Must((nome) =>
+                {
+                    bool retorno = true;
+                    foreach (char letra in nome)
+                    {
+                        if (char.IsNumber(letra) || char.IsSymbol(letra))
+                            retorno = false;
+                    }
+                    return retorno;
+                }).WithErrorCode(statusCode.DescricaoNotCaracteresEspecial);
+
+            RuleFor(z => z.IDCadeira)
+                 .NotNull()
+                .NotEmpty().WithErrorCode(statusCode.IdDeveSerInformado);
+        }
+    }
+}
