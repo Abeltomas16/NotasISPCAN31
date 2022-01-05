dd5fa7aac89dc3e87f5a641d0dbe86d23fb33dec
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2022-01-05 22:32:17 +0100

    FEAT: add loading pag e login page

---
+++ NotasISPCAN/NotasISPCAN/Views/Control/LoadingPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using NotasISPCAN.ViewModel;
+using Splat;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotasISPCAN.Views.Control
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadingPage : ContentPage
+    {
+        LoadingViewModel ViewModel { get; set; } = Locator.Current.GetService<LoadingViewModel>();
+        public LoadingPage()
+        {
+            InitializeComponent();
+            ViewModel.Init();
+        }
+    }
+}
---
+++ NotasISPCAN/NotasISPCAN/Views/Control/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using NotasISPCAN.ViewModel;
+using Rg.Plugins.Popup.Services;
+using Splat;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotasISPCAN.Views.Control
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        LoginViewModel ViewModel { get; set; } = Locator.Current.GetService<LoginViewModel>();
+        public LoginPage()
+        {
+            InitializeComponent();
+            BindingContext = ViewModel;
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+        private async void AjudaFacebook(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(new Animation.Animation(Features.Enums.EnumPerfil.Email));
+        }
+        private void MenuItem2_Clicked(object sender, EventArgs e)
+        {
+            var texto = (ToolbarItem)sender;
+            DisplayAlert("info", texto.Text, "ok");
+        }
+    }
+}
